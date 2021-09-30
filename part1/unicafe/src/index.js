48ad241949b910eecaafef5a5ad3476c1b7d0998
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2021-09-30 01:49:23 -0300

    end part1 exercise1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,7 +2,26 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = props => {
-  return <button onClick={props.handler} >{props.name}</button>
+  return <button onClick={props.handler}>{props.name}</button>
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const positive = (good / (good + neutral + bad)) * 100
+  const all = good + neutral + bad
+
+  // good => 1 , bad => -1 , neutral 0
+  const averageWeighted = (good * 1 + bad * -1 + neutral * 0) / all
+
+  return (
+    <>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {averageWeighted}</p>
+      <p>Positive: {positive} %</p>
+    </>
+  )
 }
 
 const App = () => {
@@ -11,18 +30,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const triggerGood = () => {
+    setGood(good + 1)
+  }
+  const triggerNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const triggerBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button name='good'     handler={() => {setGood( good+1)}  } />
-      <Button name='neutral'  handler={() => {setNeutral( neutral+1)}  } />
-      <Button name='bad'      handler={() => {setBad( bad+1)}  } />
+      <Button name='good' handler={triggerGood} />
+      <Button name='neutral' handler={triggerNeutral} />
+      <Button name='bad' handler={triggerBad} />
 
-    <h1>Statistics</h1>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
