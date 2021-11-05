1cebc25ca9dc3ba7bad26da495892e116d984dab
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2021-11-05 16:39:46 -0300

    fien documentacion 2c

--- part2/doc/src/App.jsx
+++ part2/doc/src/App.jsx
@@ -1,84 +1,86 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
-import noteService from './services/notes.js'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('a new note...')
-  const [showAll, setShowAll] = useState(true)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
 
-  const hook = () => {
-    //console.log('efect')
+  useEffect(() => {
     noteService
-    .getAll()
-    .then(response => {
-      setNotes(response.data)
+      .getAll()
+      .then(initialNotes => {
+      setNotes(initialNotes)
     })
-  }
+  }, [])
 
-  useEffect(hook, [])
-  //console.log('render', notes.length, 'notes')
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
-
-  const addNote = event => {
+  const addNote = (event) => {
     event.preventDefault()
-
     const noteObject = {
       content: newNote,
-      date: new Date(),
-      important: Math.random() < 0.5
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
     }
 
     noteService
-    .create(noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
-
-  }
-
-  const handleNoteChange = event => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-
+  
     noteService
     .update(id, changedNote)
-    .then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
+      .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })    
   }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+          show {showAll ? 'important' : 'all' }
         </button>
-      </div>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type='submit'>save</button>
-      </form>
-
+      </div>   
       <ul>
-        {notesToShow.map(note => (
-          <Note
-            key={note.id}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
-        ))}
+        {notesToShow.map(note => 
+            <Note
+              key={note.id}
+              note={note} 
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+        )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
--- part2/doc/src/components/Note.jsx
+++ part2/doc/src/components/Note.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Note = ({ note, toggleImportance }) => {
-  const label = note.important ? 'make not important' : 'make important'
+  const label = note.important
+    ? 'make not important' : 'make important'
 
   return (
     <li>
-      {note.content}
+      {note.content} 
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
