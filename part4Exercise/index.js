0f4e11a97da9f5a9f9f6c5e5a7b90ecc985c2206
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-25 20:18:18 -0300

    version 1.0

--- part4Exercise/index.js
+++ part4Exercise/index.js
@@ -1,11 +1,43 @@
-const app = require('./app.js')
 const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const server = http.createServer(app)
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-const PORT = config.PORT || 3003
-server.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
-}) 
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
