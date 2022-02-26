55534a274a9fdbc788601d92af4d4bb14e0b0541
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-25 21:03:39 -0300

    configuracion de lint

---
+++ part4Exercise/controllers/BlogController.js
@@ -0,0 +1,27 @@
+const blogsRouter = require('express').Router()
+
+const Blog = require('../models/Blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const notes = await Blog
+    .find({})
+
+  response.json(notes)
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes
+  })
+
+  const savedBlog = await blog.save()
+
+  response.status(201).json(savedBlog)
+})
+
+module.exports = blogsRouter
--- part4Exercise/jest.config.js
+++ part4Exercise/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    testEnvironment: 'node',
-    setupFilesAfterEnv: [
-        './jest.setup.js'
-    ]
+  testEnvironment: 'node',
+  setupFilesAfterEnv: [
+    './jest.setup.js'
+  ]
 }
