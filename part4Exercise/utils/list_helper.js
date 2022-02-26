bf11b6ca8eafa41969a0c489e385983c6d51b663
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-26 20:32:24 -0300

    end execise 4.5

--- part4Exercise/utils/list_helper.js
+++ part4Exercise/utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, item) => sum + item.likes, 0)
+}
+
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
