2b61638770ddfb50002a89fd52fa10a3e31030a3
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-26 18:43:08 -0300

    end exercise 4.1

--- part4Exercise/app.js
+++ part4Exercise/app.js
@@ -1,14 +1,18 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 const mongoose = require('mongoose')
+require('express-async-errors')
 
+// ROUTER INCLUDES
 const blogsRouter = require('./controllers/BlogController')
 
 
 const logger = require('./utils/logger')
 
+
+// START
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
@@ -19,7 +23,12 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connection to MongoDB:', error.message)
   })
 
+// set up EXPRESS
+app.use(cors())
+app.use(express.json())
 
+
+// ADD ROUTES TO EXPRESS
 app.use('/api/blogs', blogsRouter)
 
 
--- part4Exercise/controllers/BlogController.js
+++ part4Exercise/controllers/BlogController.js
@@ -10,6 +10,7 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+
   const { title, author, url, likes } = request.body
 
   const blog = new Blog({
