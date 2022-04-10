53c5a0961598fae832a9b4ea419dbde64e492302
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-04-10 19:04:05 -0300

    end exercise 5.7 - show/hide blog's detail

--- part5Exercise/src/components/Blog.js
+++ part5Exercise/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
+import { useState, useEffect } from 'react'
+import React, { useRef } from 'react'
+
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+  const toggleVisibility = () => { setVisible(!visible) }
+
+
+  const detailsOn = <div style={blogStyle}>
+    {blog.title} <button onClick={toggleVisibility}>hide</button>
+    <p>{blog.url}</p>
+    <p>like {blog.likes} <button>Like</button> </p>
+    <p>{blog.author}</p>
+  </div>
+
+  const detailsOff = <div style={blogStyle}>
     {blog.title} {blog.author}
-  </div>  
-)
+    <button onClick={toggleVisibility}>view</button>
+  </div>
+
+  return visible ? detailsOn : detailsOff
+}
 
 export default Blog
