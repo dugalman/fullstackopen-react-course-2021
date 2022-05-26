25cc24b39e10d6473afc0576f51b95a1949c52a8
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-05-26 11:40:05 -0300

    add proptypes for check properties in element

--- part5/src/components/LoginForm.jsx
+++ part5/src/components/LoginForm.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 const LoginForm = ({ handleSubmit, handleUsernameChange, handlePasswordChange, username, password }) => {
@@ -30,5 +31,14 @@ const LoginForm = ({ handleSubmit, handleUsernameChange, handlePasswordChange, u
         </div>
     )
 }
+
+LoginForm.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+}
+
 
 export default LoginForm
