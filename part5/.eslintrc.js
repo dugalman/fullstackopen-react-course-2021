a52d2e156eecf291004cdfca73b2ed79f8c4106c
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-05-26 12:55:47 -0300

    Se modifican los archivos de configuracion de eslint

--- part5/.eslintrc.js
+++ part5/.eslintrc.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   'env': {
     'browser': true,
@@ -45,7 +44,8 @@ module.exports = {
       'error', { 'before': true, 'after': true }
     ],
     'no-console': 0,
-    'react/prop-types': 0
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off'
   },
   'settings': {
     'react': {
